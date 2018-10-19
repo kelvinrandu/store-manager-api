892cb40cf4f01853d10d11b593f0d580cc9229cf
kelvinrandu <randukelvin@gmail.com>
2018-10-19 08:36:32 +0300

    [Feature #161324240] Added jwt authorization

--- application/app.py
+++ application/app.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from application.v1.resources.views import store_manager
 from instance.config import app_config
+from flask_jwt_extended import JWTManager
 
 def create_app(config):
     app = Flask(__name__)
     app.config.from_object(app_config[config])
 
     app.register_blueprint(store_manager)
+
+    app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
+    jwt = JWTManager(app)
 
     return app
 
